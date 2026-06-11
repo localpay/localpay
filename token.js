80707c7e5fd91a4585ef52d3af50075b6af4b372
komsco <150205890+localpay@users.noreply.github.com>
2026-06-11 15:29:10 +0900

    Update token.js

--- token.js
+++ token.js
@@ -1,4 +1,4 @@
-const GITHUB_TOKEN = "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"; // Personal Access Token
+const GITHUB_TOKEN = "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"; // Personal Access Token
 const GITHUB_OWNER = "localpay";          // 저장소 소유자
 const GITHUB_REPO  = "localpay";          // 저장소 이름
 const GITHUB_PATH  = "score.txt";         // 파일 경로
