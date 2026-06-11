a79f623fa9d8140c84bd8431c184d85676061c6d
komsco <150205890+localpay@users.noreply.github.com>
2026-06-11 13:43:39 +0900

    Create token.js

---
+++ token.js
@@ -0,0 +1,4 @@
+const GITHUB_TOKEN = "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"; // Personal Access Token
+const GITHUB_OWNER = "localpay";          // 저장소 소유자
+const GITHUB_REPO  = "localpay";          // 저장소 이름
+const GITHUB_PATH  = "score.txt";         // 파일 경로
