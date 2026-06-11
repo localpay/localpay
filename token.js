baa2a3865315d8a384063e723e8c6cdff094bf8d
komsco <150205890+localpay@users.noreply.github.com>
2026-06-11 15:50:37 +0900

    Change GitHub personal access token
    Updated the GitHub personal access token.

--- token.js
+++ token.js
@@ -1,4 +1,4 @@
-const GITHUB_TOKEN = "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"; // Personal Access Token
+const GITHUB_TOKEN = "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"; // Personal Access Token
 const GITHUB_OWNER = "localpay";          // 저장소 소유자
 const GITHUB_REPO  = "localpay";          // 저장소 이름
 const GITHUB_PATH  = "score.txt";         // 파일 경로
